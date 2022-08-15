9fe35c16eeb6ba4a30ff53725e5f76dfeb624ee8
Ozodbekyum <hello7ozod@gamil.com>
2022-08-15 13:39:18 +0500

    list10

--- bin/list_index10.dart
+++ bin/list_index10.dart
@@ -1,9 +1,28 @@
 /*
     Create function named func with arguments list_num
-    A list of numbers consisting of several elements is given. Return the largest between the first and last elements.
+    A list of numbers consisting of several elements is given. 
+    Return the largest between the first and last elements.
     Args:
         list_num (list): parameter
     Returns:
         list: return answer
 */
-void main() {}
+List func(List list1) {
+  List r = [];
+  r.add(list1[0]);
+  if (list1[0] > list1[list1.length - 1]) {
+    return r;
+  }
+  List x = [list1[list1.length - 1]];
+
+  return x;
+}
+
+void main() {
+  print(func([
+    5,
+    2,
+    3,
+    7,
+  ]));
+}
